69eb027c9b70c1bf891d84f2a4a8905418866ca2
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2023-10-24 14:45:55 +0900

    Gwe

--- WinApp.h
+++ WinApp.h
@@ -1,9 +1,16 @@
 #pragma once
+
+#include <Windows.h>
+
 class WinApp
 {
 
-	void Initialize();
+public:
 
+	static LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+
+	void Initialize();
 	void Update();
 
 
